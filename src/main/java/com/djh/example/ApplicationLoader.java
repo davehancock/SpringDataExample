a8a305883a6216729c7679a5102621069d158d24
daves125125 <daves125125@gmail.com>
2015-10-25 17:26:14 +0000

    adding travis

---
+++ src/main/java/com/djh/example/ApplicationLoader.java
@@ -0,0 +1,16 @@
+package com.djh.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author David Hancock
+ */
+@SpringBootApplication
+public class ApplicationLoader {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLoader.class, args);
+    }
+
+}
